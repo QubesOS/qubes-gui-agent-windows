77343e54caf262f9bd470c1334f533deb0a267f1
Marek Marczykowski <marmarek@invisiblethingslab.com>
2013-07-10 08:43:39 +0200

    Add handy build scripts, enable driver signing.
    Use the same cert as for PV drivers.
    Some part of scripts are still still stub (like 'version' file support,
    or wix packaging).

---
+++ makedist.bat
@@ -0,0 +1,45 @@
+@echo off
+
+SETLOCAL ENABLEEXTENSIONS
+IF NOT EXIST set_ddk_path.bat ECHO >set_ddk_path.bat SET DDK_PATH=C:\WinDDK\7600.16385.1
+rem IF NOT EXIST set_ddk_path_2k.bat ECHO >set_ddk_path_2k.bat SET DDK_PATH_2K=C:\WinDDK\6001.18002
+
+FOR /F %%V IN (version) DO SET VERSION=%%V
+
+SET /A NEW_BUILD_NUMBER=%BUILD_NUMBER%+1
+ECHO >build_number.bat SET BUILD_NUMBER=%NEW_BUILD_NUMBER%
+
+ECHO BUILDING %VERSION%
+
+CALL set_ddk_path.bat
+rem CALL set_ddk_path_2K.bat
+
+SET SIGNTOOL=%DDK_PATH%\bin\x86\signtool.exe
+IF NOT EXIST %SIGNTOOL% SET SIGNTOOL=%DDK_PATH%\bin\selfsign\signtool.exe
+
+SET CERT_FILENAME=
+SET CERT_PASSWORD=
+SET CERT_CROSS_CERT_FILENAME=
+SET CERT_PUBLIC_FILENAME=
+IF NOT EXIST SIGN_CONFIG.BAT GOTO DONT_SIGN
+CALL SIGN_CONFIG.BAT
+SET CERT_CROSS_CERT_FLAG=
+SET CERT_PASSWORD_FLAG=
+IF DEFINED CERT_CROSS_CERT_FILENAME SET CERT_CROSS_CERT_FLAG=/ac %CERT_CROSS_CERT_FILENAME%
+IF DEFINED CERT_PASSWORD SET CERT_PASSWORD_FLAG=-p %CERT_PASSWORD%
+IF EXIST %CERT_FILENAME% GOTO :DONT_SIGN
+"%DDK_PATH%"\bin\x86\makecert -r -pe -ss PrivateCertStore -n "CN=GPLPV Test Cert" %CERT_PUBLIC_FILENAME%
+certutil -exportpfx -user -privatekey %CERT_PASSWORD_FLAG% PrivateCertStore "GPLPV Test Cert" "%CERT_FILENAME%
+:DONT_SIGN
+
+SET QVIDEO_DIR=%CD%\qvideo
+SET AGENT_DIR=%CD%\gui-agent
+
+rem cmd /C "%DDK_PATH%\bin\setenv.bat %DDK_PATH%\ chk WIN7 && CD /D "%QVIDEO_DIR%" && build.bat && CD /D "%AGENT_DIR%" && build -cZg && cd .. && call wix.bat"
+
+cmd /C "%DDK_PATH%\bin\setenv.bat %DDK_PATH%\ chk x64 WIN7 && CD /D "%QVIDEO_DIR%" && build.bat && CD /D "%AGENT_DIR%" && build -cZg && cd .. && call wix.bat"
+
+rem cmd /C "%DDK_PATH%\bin\setenv.bat %DDK_PATH%\ fre WIN7 && CD /D "%QVIDEO_DIR%" && build -cZg && CD /D "%AGENT_DIR%" && build -cZg && cd .. && call wix.bat"
+
+rem cmd /C "%DDK_PATH%\bin\setenv.bat %DDK_PATH%\ fre x64 WIN7 && CD /D "%QVIDEO_DIR%" && build -cZg && CD /D "%AGENT_DIR%" && build -cZg && cd .. && call wix.bat"
+
---
+++ sign_config.bat
@@ -0,0 +1,4 @@
+SET CERT_FILENAME=..\winpvdrivers\cert.pfx
+SET CERT_PASSWORD=""
+rem SET CERT_CROSS_CERT_FILENAME=MS Cross Signing Certificate .cer (if you have a proper authenticode cert)
+SET CERT_PUBLIC_FILENAME=..\winpvdrivers\cert.cer
