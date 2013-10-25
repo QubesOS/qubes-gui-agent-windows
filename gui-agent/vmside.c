dc3c1571febd888066caffff5f94022752a33442
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2013-10-25 04:55:08 +0200

    Do not start multiple parallel reads on evtchn device
    If one is already pending, just reuse it. Previously some race could
    happen on OVERLAPPED object.

--- gui-agent/vmside.c
+++ gui-agent/vmside.c
@@ -624,7 +624,7 @@ ULONG WatchForEvents(
 		// read 1 byte instead of sizeof(fired_port) to not flush fired port
 		// from evtchn buffer; evtchn driver will read only whole fired port
 		// numbers (sizeof(fired_port)), so this will end in zero-length read
-		if (!ReadFile(evtchn, &fired_port, 1, NULL, &ol)) {
+		if (!bVchanIoInProgress && !ReadFile(evtchn, &fired_port, 1, NULL, &ol)) {
 			uResult = GetLastError();
 			if (ERROR_IO_PENDING != uResult) {
 				lprintf_err(uResult, "WatchForEvents(): Vchan async read");
@@ -635,8 +635,7 @@ ULONG WatchForEvents(
 
 		bVchanIoInProgress = TRUE;
 
-		if (ERROR_SUCCESS == uResult || ERROR_IO_PENDING == uResult)
-			WatchedEvents[uEventNumber++] = ol.hEvent;
+		WatchedEvents[uEventNumber++] = ol.hEvent;
 
 		dwSignaledEvent = WaitForMultipleObjects(uEventNumber, WatchedEvents, FALSE, INFINITE);
 		if (dwSignaledEvent >= MAXIMUM_WAIT_OBJECTS) {
