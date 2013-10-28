5096aabc09743fd4b0b965bd82c1d0ac616ac1b2
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2013-10-28 23:04:15 +0100

    Fix qvideo driver versioning
    This bug causes *silent* ignore of updated version.

--- set_version.ps1
+++ set_version.ps1
@@ -2,5 +2,5 @@ $version = (cat version);
 $date = get-date -format MM\/dd\/yyyy;
 # TODO: perhaps use stampinf?
 cat qvideo\inf\qvideo.inf | %{$_ -replace "^DriverVer = .*","DriverVer = $date,$version" } > qvideo\inf\qvideo.inf.version
-$version2 = %{$version -replace ".",","}
-cat qvideo\inc\qubes_common.rc.template | %{$_ -replace "VER_PRODUCTVERSION	.*","VER_PRODUCTVERSION $version"} > qvideo\inc\qubes_common.rc
+$version2 = %{$version -replace "\.",","}
+cat qvideo\inc\qubes_common.rc.template | %{$_ -replace "VER_PRODUCTVERSION	.*","VER_PRODUCTVERSION $version2"} > qvideo\inc\qubes_common.rc
