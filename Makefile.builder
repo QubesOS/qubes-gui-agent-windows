82e2d7fc44b532511c2c62aff143bf4361dac163
Rafał Wojdyła <omeg@invisiblethingslab.com>
2014-11-28 13:15:58 +0100

    added test pattern generator

--- Makefile.builder
+++ Makefile.builder
@@ -3,7 +3,7 @@ WIN_SOURCE_SUBDIRS = .
 WIN_COMPILER = WDK
 WIN_BUILD_DEPS = gui-common core-vchan-$(BACKEND_VMM)
 WIN_BUILD_DEPS += vmm-xen-windows-pvdrivers windows-utils
-WIN_PREBUILD_CMD = powershell -executionpolicy bypass set_version.ps1
+WIN_PREBUILD_CMD = powershell -executionpolicy bypass set_version.ps1 && build-test.bat
 
 # This is the simplest way to build 32-bit binaries in a 64-bit build
 # with the current builder architecture (and WDK 7.1)...
---
+++ test/pattern-generator/pattern-generator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+// Generate grid "ruler" pattern image.
+// Set as desktop wallpaper for easy window coordinate readout.
+
+namespace test_pattern
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int width, height;
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: {0} <width> <height>", Process.GetCurrentProcess().ProcessName);
+                return 1;
+            }
+            
+            width = int.Parse(args[0]);
+            height = int.Parse(args[1]);
+
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+            // grid
+            for (int x = 0; x<width; x++)
+            {
+                for (int y=0; y<height; y++)
+                {
+                    if ((x % 10) * (y % 10) == 0)
+                        bmp.SetPixel(x, y, Color.FromArgb(0x004040));
+
+                    if ((x % 50) * (y % 50) == 0)
+                        bmp.SetPixel(x, y, Color.FromArgb(0x206060));
+
+                    if ((x % 100) * (y % 100) == 0)
+                        bmp.SetPixel(x, y, Color.FromArgb(0xc04000));
+                }
+            }
+
+            // numbers
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Font font = new Font("Arial", 8);
+                SolidBrush brush = new SolidBrush(Color.White);
+                StringFormat sf = new StringFormat();
+
+                for (int x = 0; x < width; x += 100)
+                    g.DrawString(x.ToString(), font, brush, x + 2, 0);
+
+                for (int y = 100; y < height; y += 100)
+                    g.DrawString(y.ToString(), font, brush, 0, y + 2);
+            }
+
+            bmp.Save(String.Format("{0}x{1}.png", width, height), ImageFormat.Png);
+
+            return 0;
+        }
+    }
+}
